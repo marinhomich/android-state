87f7c5dd3a606d1d7c8a9f278cf1e147add843c0
Michel Marinho <michel.marinho04@outlook.com>
2023-10-06 16:46:55 -0400

    update

---
+++ app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.example.basicstatecodelab
+
+import WellnessScreen
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicStateCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable {mutableStateOf(0) }
+    StatelessCounter(count, {count++}, modifier)
+
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier){
+    Column(modifier = modifier.padding(16.dp)) {
+        if(count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10){
+            Text("Add One")
+        }
+    }
+}
+
+@Composable
+fun WellnessTasksList(
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(items = list, key = {task -> task.id}) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = {onCloseTask(task)})
+        }
+    }
+}
+
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+){
+  Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically){
+      Text(modifier = Modifier
+          .weight(1f)
+          .padding(start = 16.dp), text = taskName)
+      Checkbox(checked = checked, onCheckedChange = onCheckedChange )
+      IconButton(onClick = onClose) {
+          Icon(Icons.Filled.Close, contentDescription = "Close")
+      }
+  }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF723232,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_CAR
+)
+@Composable
+fun SurveyAnswerPreview() {
+    BasicStateCodelabTheme {
+        Row {
+            Text(text = "Spar", color = Color(0xFFEC407A))
+        }
+    }
+
+}
+
+@Composable
+fun SurveyAnswer() {
+    Row {
+        Text(text = "Spar", color = Color(0xFFEC407A))
+    }
+}
---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,30 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.basicstatecodelab.StatefulCounter
+
+import com.example.basicstatecodelab.WellnessTasksList
+
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier, wellnessViewModel: WellnessViewModel = viewModel()) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = {task -> wellnessViewModel.remove(task)} )
+    }
+}
+
